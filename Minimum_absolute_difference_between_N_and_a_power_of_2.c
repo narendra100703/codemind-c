78d7cda883ba11894d313d756de0483b439c782c
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-07-09 10:24:04 +0530

    #include<stdio.h> main() {     int number,leftpower=2,rightpower=2;     scanf("%d",&number);     while(leftpower*2<=number)     {         leftpower*=2;     }     while(rightpower<=number)     {         rightpower*=2;     }     if(number-leftpower<rightpower-number)     {         printf("%d",number-leftpower);     }     else     {         printf("%d",rightpower-number);     } }

---
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+    int number,leftpower=2,rightpower=2;
+    scanf("%d",&number);
+    while(leftpower*2<=number)
+    {
+        leftpower*=2;
+    }
+    while(rightpower<=number)
+    {
+        rightpower*=2;
+    }
+    if(number-leftpower<rightpower-number)
+    {
+        printf("%d",number-leftpower);
+    }
+    else
+    {
+        printf("%d",rightpower-number);
+    }
+}
